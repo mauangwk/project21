73afb4edece05cabe007b0fb60cd621cb5db4777
Mau Anguiano <143742379+mauangwk@users.noreply.github.com>
2024-07-22 14:08:59 -0600

    plot corrections

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("Test App")
-X = np.random.normal(0, 10, size=(100, 2))
+X = np.random.normal(0, 10, size=(10000, 2))
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 6))
 
-ax[0].hist(X[::, 0])
-ax[1].hist(X[::, 1])
+ax[0].hist(X[::, 0], color="red",
+           alpha=0.4, bins=50)
+ax[0].spines["top"].set_visible(False)
+ax[0].spines["right"].set_visible(False)
+ax[0].set_title("X1", size=14)
+
+ax[1].hist(X[::, 1], color="blue",
+           alpha=0.6, bins=50)
+ax[1].spines["top"].set_visible(False)
+ax[1].spines["right"].set_visible(False)
+ax[1].set_title("X2", size=14)
 
 st.pyplot(fig)
